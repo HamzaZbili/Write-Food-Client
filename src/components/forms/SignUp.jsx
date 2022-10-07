0dc2735dd51eb26da5c190fd379bc1da4cc2a3aa
Hamza Zbili <hamza.zbili.92@gmail.com>
2022-10-07 16:17:20 +0100

    imported service to file

--- src/components/forms/SignUp.jsx
+++ src/components/forms/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import service from "../auth/service";
 
 const SignUp = () => {
   const [user, setUser] = useState({

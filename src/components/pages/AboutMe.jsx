42962829fd8b644338a98b342f46376f51180690
Hamza Zbili <hamza.zbili.92@gmail.com>
2023-02-11 10:48:34 +0100

    added text to about me page

--- src/components/pages/AboutMe.jsx
+++ src/components/pages/AboutMe.jsx
@@ -13,10 +13,17 @@ const AboutMe = () => {
         <img src={rachel} alt="rachel" className="aboutMeImage" />
         <div className="aboutMeText">
           <h4>About me</h4>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-          omnis nobis aliquam quod vero id officia doloribus exercitationem
-          neque labore molestiae, accusantium facere? Cum porro ducimus
-          recusandae modi quo beatae.faesaesfd.
+          <br/>
+          Hi there!
+          <br/>
+          <br/>
+          I'm a passionate and experienced food writer with a diverse background in the industry. As the French Correspondent for Palate Magazine, I have the privilege of sharing my love and knowledge of the vibrant food culture of France to a wide audience. My writing also appears in Paris Unlocked, The London Women and Wine Club, and HIP Paris, where I honed my skills as an editorial assistant. In addition, I've collaborated with Dishcult UK, creating cultural and food guides for cities such as Edinburgh, Glasgow, and London.
+          <br/>
+          <br/>
+          I bring a unique perspective to my writing, having owned and operated a micro bakery, Figue Bakes, in Edinburgh. My hands-on experience in the culinary world has allowed me to connect with and understand the passion and hard work that goes into creating delicious food.
+          <br/>
+          <br/>
+          I also have a strong background in the social impact side of the food industry, having served as the Operations Lead for a large Glasgow-based foodbank. In this role, I managed the charity cafe and ensured the smooth running of operations to support those in need.
         </div>
       </div>
     </>

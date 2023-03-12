03fc806d44d3c164f3151d45e16481c86f98c058
Hamza Zbili <hamza.zbili.92@gmail.com>
2023-03-12 16:30:55 +0100

    added on hover style to article cards

--- src/components/article components/ArticleCard.jsx
+++ src/components/article components/ArticleCard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./articleCard.css";
-import { formatDate } from "../../utils/dateFormat";
 
 const ArticleCard = ({ article }) => {
-  const { title, image, link, publicationDate, publisher, other } = article;
+  const { title, image, link } = article;
 
   const openInNewTab = (url) => {
     window.open(url, "_blank", "noopener,noreferrer");
@@ -14,17 +13,19 @@ const ArticleCard = ({ article }) => {
   };
   return (
     <div className="articleContainer">
+      {}
+
       <div
         className="articleCard"
         style={{ backgroundImage: `url(${image})` }}
         onClick={handleClick}
-      ></div>
+      >
+        <div className="articleHover">
+          <p>read</p>
+        </div>
+      </div>
       <div className="articleInfo">
         <h2>{title}</h2>
-        {/* <div className="publisherAndDate">
-          {publisher ? publisher : other}
-          <h4>{formatDate(publicationDate)}</h4>
-        </div> */}
       </div>
     </div>
   );

a37ae77fdd6d6953712a55f933fe1b2f9a0413cc
Hamza Zbili <hamza.zbili.92@gmail.com>
2023-03-19 16:11:33 +0100

    modified CORS

--- src/components/auth/service.js
+++ src/components/auth/service.js
@@ -6,9 +6,11 @@ const service = axios.create({
 });
 
 service.interceptors.request.use((config) => {
+  config.headers["Access-Control-Allow-Origin"] =
+    "http://rachelnaismith.com.s3-website.eu-west-2.amazonaws.com/";
   const token = localStorage.getItem("authToken");
   config.headers.Authorization = token ? `Bearer ${token}` : "";
-  return config;
+  return config, (error) => Promise.reject(error);
 });
 
 service.isLoggedIn = async () => {
